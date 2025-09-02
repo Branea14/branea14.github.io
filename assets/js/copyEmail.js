9c2948fb7e82636ae38b332e907ae565d7d0e5a3
Erika <brandonerika96@gmail.com>
2025-09-02 14:34:27 -0400

    ul added

--- assets/js/copyEmail.js
+++
@@ -1,8 +0,0 @@
-// const copyEmail = (event) => {
-//     event.preventDefault()
-//     const email = event.currentTarget.dataset.email
-
-//     navigator.clipboard.writeText(email).then(() => {
-//         alert("Copied email: " + email)
-//     })
-// }
